eb87bccaf1c96e90777b37466bad6d106d2512b1
Christopher Blackmom <Christopher Blackmom@192.168.157.1>
2017-03-08 21:55:33 -0600

    Go Eagles!

--- src/main/java/wasdev/sample/servlet/SimpleServlet.java
+++ src/main/java/wasdev/sample/servlet/SimpleServlet.java
@@ -1,27 +1,33 @@
 package wasdev.sample.servlet;
-
 import java.io.IOException;
-
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-
+import com.ibm.watson.developer_cloud.language_translator.v2.LanguageTranslator;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.Language;
+import com.ibm.watson.developer_cloud.language_translator.v2.model.TranslationResult;
 /**
- * Servlet implementation class SimpleServlet
- */
+* Servlet implementation class SimpleServlet
+*/
 @WebServlet("/SimpleServlet")
 public class SimpleServlet extends HttpServlet {
-    private static final long serialVersionUID = 1L;
-
-    /**
-     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-     */
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        response.setContentType("text/html");
-        response.getWriter().print("Hello World!");
-    }
-
+private static final long serialVersionUID = 1L;
+/**
+* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+*/
+@Override
+protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+response.setContentType("text/html");
+response.getWriter().print(translateToSpanish("This is a test"));
+}
+private String translateToSpanish(String text){
+LanguageTranslator service = new LanguageTranslator();
+service.setUsernameAndPassword("5d25d225-fd4c-4c06-9c7c-a57981dc244a", "dr6SEYyql5dj");
+TranslationResult translationResult = service.translate(
+text, Language.ENGLISH, Language.SPANISH)
+.execute();
+return translationResult.getFirstTranslation();
+}
 }
